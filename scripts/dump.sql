c8253c299a544d589d2356d9f2655d4c267e234a
InesAguas <inocas_99@hotmail.com>
2023-10-22 18:39:18 +0200

    dump sql

--- scripts/dump.sql
+++ scripts/dump.sql
@@ -211,6 +211,14 @@ ALTER TABLE ONLY public."user" ALTER COLUMN user_id SET DEFAULT nextval('public.
 --
 
 COPY public."order" (order_id, order_time, "_Order__finish_time", user_id, order_status) FROM stdin;
+1	2023-10-22 15:54:38.81897+00	2023-10-22 15:59:45.81905+00	2	Delivered
+2	2023-10-22 15:55:56.079727+00	2023-10-22 16:08:08.079744+00	2	Delivered
+3	2023-10-22 16:07:15.178462+00	2023-10-22 16:12:08.178479+00	1	Delivered
+4	2023-10-22 16:08:26.445165+00	2023-10-22 16:17:10.445185+00	1	Delivered
+5	2023-10-22 16:10:32.982914+00	2023-10-22 16:15:54.982948+00	1	Approved
+6	2023-10-22 16:17:33.19812+00	2023-10-22 16:20:44.198152+00	1	Submitted
+7	2023-10-22 16:34:59.066862+00	2023-10-22 16:43:00.066908+00	3	Delivered
+8	2023-10-22 16:36:03.755828+00	2023-10-22 16:49:12.755862+00	3	Delivered
 \.
 
 
@@ -235,6 +243,20 @@ COPY public.plate (plate_id, plate_name, price, picture) FROM stdin;
 --
 
 COPY public.plate_order (plate_id, order_id, quantity) FROM stdin;
+7	1	3
+4	1	16
+5	1	1
+5	2	2
+7	2	2
+4	3	2
+8	3	1
+1	4	3
+6	5	3
+8	6	3
+8	7	2
+6	7	1
+2	7	1
+7	8	1
 \.
 
 
@@ -243,6 +265,12 @@ COPY public.plate_order (plate_id, order_id, quantity) FROM stdin;
 --
 
 COPY public.review (review_id, user_id, plate_id, comment, rating) FROM stdin;
+1	2	7	The pasta was overcooked.	2
+2	2	5	Could be better.	3
+3	1	1	Very good pizza.	5
+4	3	2	Not fresh.	2
+5	3	8	Very good risotto, my wife loved it.	4
+6	3	7	Pretty good, but I could do better at home.	3
 \.
 
 
@@ -251,7 +279,38 @@ COPY public.review (review_id, user_id, plate_id, comment, rating) FROM stdin;
 --
 
 COPY public."user" (user_id, username, hashed_password) FROM stdin;
+1	ines	1a05f59fc22e9a5e5cbcb25d5c0723e558166728e6e118289ce83795c51a9f59
+2	meli	ebdf496f67651cddf6aaa1f0b130f1b99ce9e2e93dc2503d926edcff15aee668
+3	john	bfbfe08edb20e274563cd69250f4e1be3c17bbb919a68082b7fb82802e10ffe0
 \.
+
+
+--
+-- Name: order_order_id_seq; Type: SEQUENCE SET; Schema: public; Owner: tech_user
+--
+
+SELECT pg_catalog.setval('public.order_order_id_seq', 8, true);
+
+
+--
+-- Name: plate_plate_id_seq; Type: SEQUENCE SET; Schema: public; Owner: tech_user
+--
+
+SELECT pg_catalog.setval('public.plate_plate_id_seq', 1, false);
+
+
+--
+-- Name: review_review_id_seq; Type: SEQUENCE SET; Schema: public; Owner: tech_user
+--
+
+SELECT pg_catalog.setval('public.review_review_id_seq', 6, true);
+
+
+--
+-- Name: user_user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: tech_user
+--
+
+SELECT pg_catalog.setval('public.user_user_id_seq', 3, true);
 
 
 --
